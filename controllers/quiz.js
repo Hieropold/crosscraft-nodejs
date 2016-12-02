8103a75e2a7d47401cc5b964360c6b3e93e571c5
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2016-12-02 16:33:06 +0300

    Refactor word factory to use EC6 class.

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,18 +1,15 @@
 'use strict';
 
-var async = require('async');
-var Promise = require('bluebird');
-var log = require('npmlog');
-var word = require(__dirname + '/../models/word.js');
+let Promise = require('bluebird');
+let log = require('npmlog');
+let word = require(__dirname + '/../models/word.js');
 
 module.exports.create = create;
 
 function create(app, preproc) {
     app.get('/quiz', preproc, function (req, res) {
+
         word.getRandomWord()
-            .then(function (randomWord) {
-                return word.attachCluesList(randomWord);
-            })
             .then(function (word) {
                 res.render('pages/quiz', {
                     'word': word.word,
@@ -27,8 +24,8 @@ function create(app, preproc) {
     });
 
     app.get('/quiz/answer/:wid/:cid', preproc, function (req, res) {
-        var wid = parseInt(req.params.wid);
-        var cid = parseInt(req.params.cid);
+        let wid = parseInt(req.params.wid);
+        let cid = parseInt(req.params.cid);
 
         // Validate params
         if (!wid || !cid) {
@@ -40,16 +37,17 @@ function create(app, preproc) {
             word.getClueById(cid)
         ])
             .then(function (data) {
-                var word = data[0];
-                var clue = data[1];
+                let word = data[0];
+                let clue = data[1];
 
                 if (word === false || clue === false) {
                     return res.status(404).send('Not found');
                 }
 
-                if (word.wid && clue.wid && word.wid == clue.wid) {
+                if (word.isClueCorrect(clue)) {
                     return res.render('pages/success');
                 }
+
                 return res.render('pages/fail');
             })
             .catch(function (err) {
--- models/word.js
+++ models/word.js
@@ -1,13 +1,57 @@
 'use strict';
 
-var Promise = require('bluebird');
-
-var db = require(__dirname + '/db.js');
+let Promise = require('bluebird');
+let db = require(__dirname + '/db.js');
 
 module.exports.getRandomWord = getRandomWord;
-module.exports.attachCluesList = attachCluesList;
-module.exports.getClueById = getClueById;
 module.exports.getWordById = getWordById;
+module.exports.getClueById = getClueById;
+
+class Word {
+    constructor(wid, word) {
+        this.wid = wid;
+        this.word = word;
+    }
+
+    attachClues(clues) {
+        this.clues = clues;
+    }
+
+    isClueCorrect(clue) {
+        return this.wid === clue.wid;
+    }
+}
+
+function getRandomWord() {
+    let word;
+
+    return getRandomOffset()
+        .then(function (offset) {
+            return getWordByOffset(offset);
+        })
+        .then(function (row) {
+            word = new Word(row.wid, row.word);
+            return fetchCluesList(word);
+        })
+        .then(function (clues) {
+            word.attachClues(clues);
+            return word;
+        });
+}
+
+function getRandomOffset() {
+    return db.query('SELECT FLOOR(RAND() * COUNT(*)) AS offset FROM words')
+        .then(function (rows) {
+            return rows[0].offset;
+        });
+}
+
+function getWordByOffset(offset) {
+    return db.query('SELECT * FROM words LIMIT ?, 1', [offset])
+        .then(function (rows) {
+            return rows[0];
+        });
+}
 
 function getClueById(id) {
     return db.query('SELECT * FROM clues WHERE cid = ?', [id])
@@ -27,45 +71,22 @@ function getWordById(id) {
                 return false;
             }
 
-            return rows[0];
+            return new Word(rows[0].wid, rows[0].word);
         });
 }
 
-function getRandomWord() {
-    return getRandomOffset()
-        .then(function (offset) {
-            return getWordByOffset(offset);
-        });
-}
-
-function attachCluesList(word) {
+function fetchCluesList(word) {
     return calcCluesCount()
         .then(function (totalClues) {
-            var clues = [];
-            for (var i = 0; i < 5; i++) {
+            let clues = [];
+            for (let i = 0; i < 5; i++) {
                 clues.push(getRandomClue(totalClues));
             }
             clues.push(getClueByWordId(word.wid));
             return Promise.all(clues);
         })
         .then(function (clues) {
-            word.clues = shuffleArray(clues);
-
-            return word;
-        });
-}
-
-function getRandomOffset() {
-    return db.query('SELECT FLOOR(RAND() * COUNT(*)) AS offset FROM words')
-        .then(function (rows) {
-            return rows[0].offset;
-        });
-}
-
-function getWordByOffset(offset) {
-    return db.query('SELECT * FROM words LIMIT ?, 1', [offset])
-        .then(function (rows) {
-            return rows[0];
+            return shuffleArray(clues);
         });
 }
 
@@ -84,7 +105,7 @@ function getClueByWordId(wordId) {
 }
 
 function getRandomClue(totalClues) {
-    var randomOffset = parseInt(Math.random() * totalClues);
+    let randomOffset = parseInt(Math.random() * totalClues);
     return db.query('SELECT cid, clue FROM clues LIMIT ?, 1', [randomOffset])
         .then(function (rows) {
             return rows[0];
@@ -97,9 +118,9 @@ function getRandomClue(totalClues) {
  * @description Durstenfield shuffle implementation.
  */
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
